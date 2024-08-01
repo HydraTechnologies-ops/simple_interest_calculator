785c45a7254c4ce5e85ec514f52214cec2c24682
GammaWraith <kahlob.bookout06@gmail.com>
2024-07-31 23:34:17 -0500

    commit to main

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function compute()
     p = document.getElementById("principal").value;
 
 }
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
