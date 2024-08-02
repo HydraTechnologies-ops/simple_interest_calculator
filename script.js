3b489ff4a12fa54ef850550ef81cdd1b00d990f1
GammaWraith <kahlob.bookout06@gmail.com>
2024-08-02 10:17:11 -0500

    commit to main

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute() {
+  let p = document.getElementById("principal").value;
+  let principal = parseInt(p);
+  let rate = parseInt(document.getElementById("rate").value);
+  let years = parseInt(document.getElementById("years").value);
+  let interest = parseFloat((principal * years * rate) / 100);
+  let amount = principal + interest;
+  let result = document.getElementById("result");
+  let year = new Date().getFullYear() + years;
 
+  if (principal <= 0) {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+  } else {
+    result.innerHTML =
+      "If you deposit $" +
+      "<mark>" +
+      principal +
+      "</mark>" +
+      " , <br> at an interest rate of " +
+      "<mark>" +
+      rate +
+      "</mark>" +
+      "%. <br> You will receive an amount of $" +
+      "<mark>" +
+      interest +
+      "</mark>" +
+      ", <br> in the year + " +
+      "<mark>" +
+      year +
+      "</mark>" +
+      "<br>";
+  }
 }
 
 function updateRate() {
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval;
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval;
 }
